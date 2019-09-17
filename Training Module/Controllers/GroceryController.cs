da15e76c2fc04eb9b1684fc97872425958cb962b
richard <rober260@mail.nmc.edu>
2019-09-17 13:36:54 -0700

    first working list

---
+++ Training Module/Controllers/GroceryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Training_Module.Models;
+namespace Training_Module.Controllers
+{
+    public class GroceryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Groceries()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            List<Groceries> grocery = new List<Groceries>();
+            grocery.Add(new Groceries
+            {
+                GrabbedAlready = false,
+                ItemName = "avacado",
+                Quanitity = 4,
+                Price = 1.99,
+                Important = true,
+                Location = "produce"
+            });
+          
+            return View(grocery);
+        }
+    }
+
+}
