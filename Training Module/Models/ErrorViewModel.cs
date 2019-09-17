27a3bc7acabb3dd56a05f0f94f7868277888b006
Chauncey Kelly <cjkelly221@gmail.com>
2019-09-17 14:34:57 -0400

    First Commit. No changes made to repo

---
+++ Training Module/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Training_Module.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Training Module/Models/Groceries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Training_Module.Models
+{
+    public class Groceries
+    {
+
+        public string ItemName { get; set; }
+        public int Quanitity { get; set; }
+        public double Price { get; set; }
+        public string Location {get; set; }
+        public bool  GrabbedAlready { get; set; }
+        public bool Important { get; set; }
+    }
+
+
+}
